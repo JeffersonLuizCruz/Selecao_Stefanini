55147295cb13cc22e2713cb02bf2218b08a4fc84
Vinicius Barbosa <vini.barpo@gmail.com>
2021-09-17 01:55:38 -0300

    Integração Front-Back Upload

---
+++ Backend/spring-boot/src/main/java/com/spring/SpringBootUploadFilesApplication.java
@@ -0,0 +1,26 @@
+package com.spring;
+
+import javax.annotation.Resource;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.spring.service.FilesStorageService;
+
+
+@SpringBootApplication
+public class SpringBootUploadFilesApplication implements CommandLineRunner {
+  @Resource
+  FilesStorageService storageService;
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringBootUploadFilesApplication.class, args);
+  }
+
+  @Override
+  public void run(String... arg) throws Exception {
+    storageService.deleteAll();
+    storageService.init();
+  }
+}
